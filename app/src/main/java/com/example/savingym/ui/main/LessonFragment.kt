393f04747ddc69faa231cf22a36eaac3ef246397
bt64341 <bt64341@yandex.ru>
2020-02-26 11:20:11 +0300

    work on session 3

---
+++ app/src/main/java/com/example/savingym/ui/main/LessonFragment.kt
@@ -0,0 +1,18 @@
+package com.example.savingym.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arellomobile.mvp.MvpAppCompatFragment
+import com.example.savingym.R
+
+class LessonFragment :MvpAppCompatFragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lesson, container, false)
+    }
+}
